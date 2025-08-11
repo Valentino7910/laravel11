ce557a0059895a4d99e3c304071d0295974733c5
Valentino7910 <valentinothomassaputra79@gmail.com>
2025-08-11 15:30:29 +0700

    Add post model

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
@@ -10,51 +11,10 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Valentino', 'title' => 'About']);
 });
 Route::get('/posts', function () {
-    return view('posts', ['title' => 'Blog', 'posts' => [
-        [
-            'id' => 1,
-            'slug' => 'first-post',
-            'title' => 'First Post',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque totam tenetur ullam
-            illo soluta. Non soluta ipsum officiis maiores, ullam illum beatae eos qui eaque odio minus, culpa quibusdam
-            consequatur!',
-            'author' => 'Valentino'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'second-post',
-            'title' => 'Second Post',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vel facilis
-            numquam et quos maiores nemo, quasi deserunt debitis officia assumenda quisquam accusamus corporis inventore
-            est quo fugit nam quaerat?',
-            'author' => 'Valentino'
-        ],
-    ]]);
+    return view('posts', ['title' => 'Blog', 'posts' => Post::all()]);
 });
 Route::get('/posts/{slug}', function ($slug) {
-    $posts = [
-        [
-            'id' => 1,
-            'slug' => 'first-post',
-            'title' => 'First Post',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque totam tenetur ullam
-            illo soluta. Non soluta ipsum officiis maiores, ullam illum beatae eos qui eaque odio minus, culpa quibusdam
-            consequatur!',
-            'author' => 'Valentino'
-        ],
-        [
-            'id' => 2,
-            'slug' => 'second-post',
-            'title' => 'Second Post',
-            'content' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nesciunt vel facilis
-            numquam et quos maiores nemo, quasi deserunt debitis officia assumenda quisquam accusamus corporis inventore
-            est quo fugit nam quaerat?',
-            'author' => 'Valentino'
-        ],
-    ];
-    $post = Arr::first($posts, function ($post) use ($slug) {
-        return $post['slug'] == $slug;
-    });
+    $post = Post::find($slug);
     return view('post', [
         'title' => $post['title'],
         'post' => $post
